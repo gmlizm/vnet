ee6ca4c899e9d0ec52c89b7db6ae1c8343d36bc5
gmlizm <gmlizm@gmail.com>
2018-05-08 21:16:56 +0800

    提交

---
+++ app/web/main/src/main/java/com/aboo/vnet/web/main/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.aboo.vnet.web.main.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aboo.vnet.core.service.UserService;
+
+@RestController
+public class TestController {
+
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/stest")
+	public String test() {
+		String ss = userService.test();
+		return ss;
+	}
+}
